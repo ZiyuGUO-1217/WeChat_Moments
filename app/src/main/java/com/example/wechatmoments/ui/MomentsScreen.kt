ab3df72d909a784470facfdd26bb75f48b9a39df
Ziyu Guo <ziyu_guo@outlook.com>
2021-12-01 17:21:14 +0800

    1. modify the MultiLineStateText

--- app/src/main/java/com/example/wechatmoments/ui/MomentsScreen.kt
+++ app/src/main/java/com/example/wechatmoments/ui/MomentsScreen.kt
@@ -1,16 +1,38 @@
 package com.example.wechatmoments.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.example.wechatmoments.MomentsViewModel
+import com.example.wechatmoments.R
 import com.example.wechatmoments.collectAsState
+import com.example.wechatmoments.model.Image
+import com.example.wechatmoments.model.Sender
+import com.example.wechatmoments.model.Tweet
+import com.example.wechatmoments.ui.theme.Shapes
 import com.example.wechatmoments.ui.theme.WeChatMomentsTheme
-import com.example.wechatmoments.ui.weight.BasicTweetCell
+import com.example.wechatmoments.ui.weight.ImageGrid
+import com.example.wechatmoments.ui.weight.MultiLineStateText
 
 @Composable
 fun MomentsScreen(viewModel: MomentsViewModel = hiltViewModel()) {
@@ -23,6 +45,136 @@ fun MomentsScreen(viewModel: MomentsViewModel = hiltViewModel()) {
                 time = "1 minute ago"
             )
         }
+    }
+}
+
+@Composable
+fun BasicTweetCell(tweet: Tweet, time: String) {
+    Column {
+        CellContent(tweet, time)
+        Divider(modifier = Modifier.fillMaxWidth(), color = Color.LightGray.copy(alpha = 0.5f))
+    }
+}
+
+@Composable
+private fun CellContent(tweet: Tweet, time: String) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth()
+            .padding(top = 20.dp, bottom = 16.dp),
+        verticalAlignment = Alignment.Top
+    ) {
+        ProfileImage(tweet.sender.avatar)
+        TweetDetails(
+            userName = tweet.sender.nickName,
+            tweetContent = tweet.content,
+            imageList = tweet.images,
+            time = time
+        )
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+private fun ProfileImage(imageUrl: String) {
+    val painter = rememberImagePainter(
+        data = imageUrl,
+        builder = {
+            placeholder(R.drawable.defaultprofileimage)
+        }
+    )
+
+    Image(
+        painter = painter,
+        contentDescription = "profile image",
+        modifier = Modifier
+            .size(48.dp)
+            .clip(Shapes.medium),
+    )
+}
+
+@Composable
+private fun TweetDetails(
+    userName: String,
+    tweetContent: String,
+    imageList: List<Image>,
+    time: String
+) {
+    Column(modifier = Modifier.padding(start = 12.dp)) {
+        UserName(userName)
+        if (tweetContent.isNotBlank()) TweetContent(tweetContent)
+        if (imageList.isNotEmpty()) Images(imageList)
+        TimeAndMore(time)
+    }
+}
+
+@Composable
+private fun UserName(userName: String) {
+    Text(
+        modifier = Modifier.padding(bottom = 8.dp),
+        text = userName,
+        fontSize = 17.sp,
+        color = Color(0XFF576B95),
+        fontWeight = FontWeight.SemiBold
+    )
+}
+
+@Composable
+private fun TweetContent(tweetContent: String) {
+    MultiLineStateText(tweetContent)
+}
+
+@Composable
+private fun Images(imageList: List<Image>) {
+    ImageGrid(imageList)
+    Spacer(modifier = Modifier.padding(bottom = 12.dp))
+}
+
+@Composable
+private fun TimeAndMore(time: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = time, color = Color.Gray)
+        Box(
+            modifier = Modifier
+                .clip(Shapes.medium)
+                .background(Color.LightGray.copy(alpha = 0.5f)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                modifier = Modifier.padding(horizontal = 4.dp),
+                painter = painterResource(id = R.drawable.ic_more_horiz),
+                contentDescription = "more",
+                tint = Color(0XFF576B95)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BasicTweetCellPreview() {
+    WeChatMomentsTheme {
+        BasicTweetCell(
+            tweet = Tweet(
+                content = "This is the first tweet",
+                sender = Sender(
+                    nickName = "Nick name",
+                    userName = "User name"
+                ),
+                images = listOf(
+                    Image(url = "fake"),
+                    Image(url = "fake"),
+                    Image(url = "fake"),
+                    Image(url = "fake")
+                )
+            ),
+            time = "1 day ago"
+        )
     }
 }
 
--- app/src/main/java/com/example/wechatmoments/ui/weight/MultiLineStateText.kt
+++ app/src/main/java/com/example/wechatmoments/ui/weight/MultiLineStateText.kt
@@ -1,5 +1,6 @@
 package com.example.wechatmoments.ui.weight
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -8,13 +9,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.wechatmoments.R
+import com.example.wechatmoments.ui.theme.tertiaryBlue
+
+private const val DEFAULT_MAX_LINES = 5
 
 @Composable
 fun MultiLineStateText(tweetContent: String) {
     val (lineState, setLineState) = remember { mutableStateOf(LineState.DEFAULT) }
-    val buttonText = if (lineState == LineState.CLIP) "Expand" else "Clip"
+    val buttonText = if (lineState == LineState.CLIP) {
+        stringResource(R.string.moments_tweet_content_expand)
+    } else {
+        stringResource(R.string.moments_tweet_content_clip)
+    }
+    val onClick = {
+        when (lineState) {
+            LineState.CLIP -> setLineState(LineState.EXPAND)
+            LineState.EXPAND -> setLineState(LineState.CLIP)
+            else -> Log.d("lineState:", "$lineState")
+        }
+    }
 
     Column {
         Text(
@@ -30,21 +46,16 @@ fun MultiLineStateText(tweetContent: String) {
             Text(
                 modifier = Modifier
                     .padding(bottom = 8.dp)
-                    .clickable {
-                        when (lineState) {
-                            LineState.CLIP -> setLineState(LineState.EXPAND)
-                            LineState.EXPAND -> setLineState(LineState.CLIP)
-                        }
-                    },
+                    .clickable { onClick.invoke() },
                 text = buttonText,
-                color = Color(0xFF4c6e92)
+                color = tertiaryBlue
             )
         }
     }
 }
 
 enum class LineState(val maxLines: Int) {
-    DEFAULT(5),
-    CLIP(5),
+    DEFAULT(DEFAULT_MAX_LINES),
+    CLIP(DEFAULT_MAX_LINES),
     EXPAND(Int.MAX_VALUE)
 }
